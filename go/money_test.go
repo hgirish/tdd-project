f3bc5a5cd6e76013694a80adafcbb365d91f3733
hgirish <girish@post.com>
2021-10-25 21:50:23 -0700

    feat: add currency

--- go/money_test.go
+++ go/money_test.go
@@ -4,20 +4,37 @@ import (
 	"testing"
 )
 
-func TestMultiplication(t *testing.T) {
-	fiver := Dollar{
-		amount: 5,
+func TestMultiplicationInDollars(t *testing.T) {
+	fiver := Money{
+		amount:   5,
+		currency: "USD",
 	}
 	tenner := fiver.Times(2)
 	if tenner.amount != 10 {
 		t.Errorf("Expected 10, got: [%d]", tenner.amount)
 	}
+	if tenner.currency != "USD" {
+		t.Errorf("Expected USD, got [%s]", tenner.currency)
+	}
 }
 
-type Dollar struct {
-	amount int
+func TestMultiplicationInEuros(t *testing.T) {
+	tenEuros := Money{amount: 10, currency: "EUR"}
+	twentyEuros := tenEuros.Times(2)
+
+	if twentyEuros.amount != 20 {
+		t.Errorf("Expected 20, got [%d]", twentyEuros.amount)
+	}
+	if twentyEuros.currency != "EUR" {
+		t.Errorf("Expected EUR, got [%s]", twentyEuros.currency)
+	}
 }
 
-func (d Dollar) Times(multiplier int) Dollar {
-	return Dollar{amount: d.amount * multiplier}
+type Money struct {
+	amount   int
+	currency string
+}
+
+func (m Money) Times(multiplier int) Money {
+	return Money{amount: m.amount * multiplier, currency: m.currency}
 }
